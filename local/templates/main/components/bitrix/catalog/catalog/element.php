4c36d06a98e70efd520949518ac4f9c1a806e2c7
Kirill Terentiev <kiri4tanker@mail.ru>
2022-06-29 17:51:17 +0200

    +fix

--- local/templates/main/components/bitrix/catalog/catalog/element.php
+++ local/templates/main/components/bitrix/catalog/catalog/element.php
@@ -20,33 +20,6 @@ $this->setFrameMode(true);
 $this->addExternalCss('/bitrix/css/main/bootstrap.css');
 ?>
 
-<?
-$sectionListParams = array(
-	"SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
-	"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-	"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-	"CACHE_TYPE" => $arParams["CACHE_TYPE"],
-	"CACHE_TIME" => $arParams["CACHE_TIME"],
-	"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-	"COUNT_ELEMENTS" => $arParams["SECTION_COUNT_ELEMENTS"],
-	"TOP_DEPTH" => $arParams["SECTION_TOP_DEPTH"],
-	"SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
-	"VIEW_MODE" => $arParams["SECTIONS_VIEW_MODE"],
-	"SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
-	"HIDE_SECTION_NAME" => (isset($arParams["SECTIONS_HIDE_SECTION_NAME"]) ? $arParams["SECTIONS_HIDE_SECTION_NAME"] : "N"),
-	"ADD_SECTIONS_CHAIN" => (isset($arParams["ADD_SECTIONS_CHAIN"]) ? $arParams["ADD_SECTIONS_CHAIN"] : '')
-);
-
-$APPLICATION->IncludeComponent(
-	"bitrix:catalog.section.list",
-	"catalog_list",
-	$sectionListParams,
-	$component,
-);
-
-unset($sectionListParams);
-?>
-
 <? $APPLICATION->IncludeComponent(
 	'bitrix:catalog.section',
 	'catalog_items',
--- local/templates/main/components/bitrix/catalog/catalog/sections.php
+++ local/templates/main/components/bitrix/catalog/catalog/sections.php
@@ -17,32 +17,29 @@ $this->setFrameMode(true);
 
 <div class="filter__catalog">
     <div class="list-group">
-		 <? $sectionListParams_two = array(
-			 "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-			 "IBLOCK_ID" => $arParams["IBLOCK_ID"],
-			 "CACHE_TYPE" => $arParams["CACHE_TYPE"],
-			 "CACHE_TIME" => $arParams["CACHE_TIME"],
-			 "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-			 "COUNT_ELEMENTS" => $arParams["SECTION_COUNT_ELEMENTS"],
-			 "TOP_DEPTH" => 1,
-			 "SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
-			 "SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
-			 "VIEW_MODE" => $arParams["SECTIONS_VIEW_MODE"],
-			 "SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
-			 "HIDE_SECTION_NAME" => (isset($arParams["SECTIONS_HIDE_SECTION_NAME"]) ? $arParams["SECTIONS_HIDE_SECTION_NAME"] : "N"),
-			 "ADD_SECTIONS_CHAIN" => (isset($arParams["ADD_SECTIONS_CHAIN"]) ? $arParams["ADD_SECTIONS_CHAIN"] : '')
-		 );
-
-		 $APPLICATION->IncludeComponent(
+		 <? $APPLICATION->IncludeComponent(
 			 "bitrix:catalog.section.list",
 			 "catalog_list",
-			 $sectionListParams_two,
+			 array(
+				 "PATH_TO_SECTION_LIST" => $arParams["PATH_TO_SECTION_LIST"],
+				 "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+				 "SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
+				 "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+				 "TOP_DEPTH" => 1,
+				 "CACHE_TYPE" => $arParams["CACHE_TYPE"],
+				 "CACHE_TIME" => $arParams["CACHE_TIME"],
+				 "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+				 "COUNT_ELEMENTS" => $arParams["SECTION_COUNT_ELEMENTS"],
+				 "VIEW_MODE" => $arParams["SECTIONS_VIEW_MODE"],
+				 "SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
+				 "HIDE_SECTION_NAME" => (isset($arParams["SECTIONS_HIDE_SECTION_NAME"]) ? $arParams["SECTIONS_HIDE_SECTION_NAME"] : "N"),
+				 "ADD_SECTIONS_CHAIN" => (isset($arParams["ADD_SECTIONS_CHAIN"]) ? $arParams["ADD_SECTIONS_CHAIN"] : ''),),
 			 $component,
-			 ($arParams["SHOW_TOP_ELEMENTS"] !== "N" ? array("HIDE_ICONS" => "Y") : array())
 		 );
 
-
-		 unset($sectionListParams);
+		 unset($sectionListParams_two);
 		 ?>
     </div>
 </div>
+
+
